d318de9eb53be4a83c17393b7d7790edacabf143
altsheets <altsheets+coding@gmail.com>
2016-02-02 18:47:35 +0100

    into eclipse

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
+# See https://www.youtube.com/watch?v=kNke39OZ2k0 for a 15 minute intro
+
 import click
 
 @click.command()
-def cli():
-	"""This hello'es something."""
-	print "hello world!"
+@click.argument('greetee', default='World', required=False)
+def cli(greetee):
+	"""This hello'es something, or someone."""
+	print "Hello %s!" % greetee
--- setup.py
+++ setup.py
@@ -13,6 +13,24 @@ setup(
 		[console_scripts]
 		hello=hello:cli
 		altcoin=altcoin:cli
-	'''
+	''',
+	
+	# metadata for upload to PyPI
+  author = "AltSheets Dev",
+  author_email = "altsheets+coding@gmail.com",
+  description = "CLI client for AltFolio dataserver.",
+  license = "TODO. Probably something alike http://altsheets.ddns.net/assetgraphs/v2/LICENSE.txt",
+  keywords = "altcoins altcoin cryptocurrency cryptocurrencies bitcoin api trading data aggregator",
+  url = "http://altfolio.ddns.net/",   
+  classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        ],
+	long_description = ("altcoin price"
+										"altcoin price DOGE"
+										"altcoin price --exchange bittrex DOGE"
+										"altcoin cmc --param volume24 DOGE"
+										"altcoin cmc --param price doge"
+										),
 )
 
